13d336d12582b982f53697f99ea763c6cde90ebc
Juan Avelino <javelino@dinersclub.com.ec>
2022-03-02 18:25:04 -0500

    se actualiza servicio 2

--- src/main/java/Service2.java
+++ src/main/java/Service2.java
@@ -4,6 +4,8 @@ public class Service2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inicio de proceso 2");
+		// logica del proceso
+		System.out.println("Fin de proceso 2");
 	}
 
 }
