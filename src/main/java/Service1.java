92ff3352ed18d9b4310066a5f5e2ee8a3801e689
juanAvelino <96070602+juanAvelino@users.noreply.github.com>
2022-03-02 23:35:26 +0000

    se resuelve fix/qa-001 (#3)

--- src/main/java/Service1.java
+++ src/main/java/Service1.java
@@ -4,6 +4,8 @@ public class Service1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inicio de proceso");
+		// logica de negocio
+		System.out.println("Fin de proceso");
 	}
 
 }
