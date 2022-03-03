7c29f88cebf5d9d63f0127ab474e871899b99542
juanAvelino <96070602+juanAvelino@users.noreply.github.com>
2022-03-03 00:01:14 +0000

    DIN-11111: fix/calidad-001 (#4)
    se corrige error reportado por calidad

--- src/main/java/Service1.java
+++ src/main/java/Service1.java
@@ -3,9 +3,9 @@ public class Service1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Inicio de proceso");
+		System.out.println("Inicio de proceso 1");
 		// logica de negocio
-		System.out.println("Fin de proceso");
+		System.out.println("Fin de proceso 1");
 	}
 
 }
