78b821f667c7d6f43f186ff0c9574df1f80cd3f5
juanAvelino <96070602+juanAvelino@users.noreply.github.com>
2022-03-03 00:10:07 +0000

    DIN-11111: fix/gsf-001 (#5)
    se corrige error reportado por gsf

--- src/main/java/Service1.java
+++ src/main/java/Service1.java
@@ -3,9 +3,10 @@ public class Service1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Inicio de proceso 1");
+		System.out.println("Inicio de proceso 1.");
 		// logica de negocio
-		System.out.println("Fin de proceso 1");
+		// se modifica logica de negocio
+		System.out.println("Fin de proceso 1.");
 	}
 
 }
